bb8609ebc511fe271f24667f72266fa0dae38b37
Anton Darenskikh <darenskikh.spb@yandex.ru>
2024-04-10 17:22:25 +0300

    init v1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RESTController.java
@@ -0,0 +1,38 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
+import ru.kata.spring.boot_security.demo.services.UserServiceImpl;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class RESTController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public RESTController(UserServiceImpl userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public User getUser(@PathVariable long id) {
+        return userService.findUserById(id);
+    }
+
+    @GetMapping(value = "/users")
+    public List<User> getAllusers() {
+        return userService.allUsers();
+    }
+
+    @PostMapping(value = "/users")
+    public User addUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+}
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,3 @@
+console.log('xd')
+document.body.style.background = "red";
+setTimeout(() => document.body.style.background = 'white', 3000);
